4022ff56b02e001f6d58970690534a6f92d61f0e
Aartisoni3515 <aartisoni3517@gmail.com>
2023-01-27 16:31:40 +0530

    msg

--- src/Components/Header/Contact/Contact.js
+++ src/Components/Header/Contact/Contact.js
@@ -1,11 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Contact.css";
+import { IoLocationSharp } from "react-icons/io5";
+import { CgMail } from "react-icons/cg";
+import {
+  BsFillTelephoneFill,
+  BsInstagram,
+  BsFacebook,
+  BsLinkedin,
+  BsGithub,
+} from "react-icons/bs";
 
 const Contact = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Contact
+  return (
+    <>
+      <div className="contact-container">
+        <h1>
+          <span>C</span>ontact
+          <span>M</span>e
+        </h1>
+        <p>Get In Touch</p>
+        <div className="icons">
+          <div>
+            <IoLocationSharp style={{ fontSize: "25px" }} />
+            <h5>
+              Jaipur <br /> Rajasthan
+            </h5>
+          </div>
+          <div>
+            {" "}
+            <BsFillTelephoneFill style={{ fontSize: "20px" }} />
+            <h5>
+              {" "}
+              mobile <br />
+              7357878964
+            </h5>
+          </div>
+          <div>
+            <CgMail style={{ fontSize: "27px" }} />
+            <h5>
+              aartisoni3517 <br />
+              @gmail.com
+            </h5>
+          </div>
+        </div>
+      </div>
+
+        {/* <BsInstagram />
+        <BsFacebook />
+        {/* BsLink45Deg */}
+        {/* <BsLinkedin />
+        <BsGithub />  */}
+
+        <div className="contact-form">
+        <h2 style={{textAlign:"left"}}>Contact Form</h2>
+          <form>
+          <div className="form">
+            <div className="contact-left">
+              <div   >
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" required />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" required />
+              </div>
+              <div>
+                <label htmlFor="Phone">Phone:</label>
+                <input type="number" id="number" required />
+              </div>
+            </div>
+
+            <div className="contact-right">
+              <div>
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" required />
+              </div>
+            </div>
+            </div>
+            <button type="submit">btn</button>
+          </form>
+        </div>
+    </>
+  );
+};
+
+export default Contact;
--- src/Components/Header/Projects/Projects.js
+++ src/Components/Header/Projects/Projects.js
@@ -6,46 +6,39 @@ const Projects = () => {
     {
       ProjectName: "Amazon-clone Project",
       Desc: "This Project is a clone of Amazon. It is Build Using Reactjs,HTML,CSS, JavaScript and other react libraries.",
-      rating: "34565",
-      price: "4534",
       image:
         "https://www.lifewire.com/thmb/nfg7SU0_fIU7yOBSw5wD2zc_y0I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Webp.net-gifmaker-5addf59fba61770036c5836f.gif",
     },
+
     {
       ProjectName: "Chat-App Project",
       Desc: "This Project is a chat-app using firebase authentication. It is Build Using Reactjs,HTML,CSS, JavaScript and other react libraries.",
-      rating: "34565",
-      price: "4534",
       image: "https://blog.flock.com/hubfs/GIFs/chat.gif",
     },
+
     {
       ProjectName: "E-commerce-app Project",
       Desc: "This Project is a E-commerce platform. It is Build Using Reactjs,HTML,CSS, JavaScript and other react libraries.",
-      rating: "34565",
-      price: "4534",
       image: "https://media1.giphy.com/media/GsZWKbRmg9lI6OCdxJ/giphy.gif",
     },
+
     {
       ProjectName: "Zomato-app clone Project",
       Desc: "This Project is a clone of Zomato.It is Build Using Reactjs,HTML,CSS, JavaScript and other react libraries.",
-      rating: "34565",
-      price: "4534",
       image:
         "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/967f19111202195.5ffdfc0e915cb.gif",
     },
+
     {
       ProjectName: "Travel-blog Project",
       Desc: "This Project is about the travel sites. It is Build Using Reactjs,HTML,CSS, JavaScript and other react libraries.",
-      rating: "34565",
-      price: "4534",
       image:
         "https://i.gifer.com/origin/3d/3d6c4ddc789e0700a31da65d3684bc3a.gif",
     },
+
     {
       ProjectName: "Single-page website Project",
       Desc: "This Project is a website .It is Build Using Reactjs,HTML,CSS, JavaScript and other react libraries.",
-      rating: "34565",
-      price: "4534",
       image:
         "https://cdn.dribbble.com/users/1106204/screenshots/7063847/media/dc64a6ac0e324de8aaceec810846fff7.gif",
     },
@@ -56,35 +49,31 @@ const Projects = () => {
       <h2
         style={{
           textAlign: "center",
-          marginBottom: "60px",
-          marginLeft:"30%",
+          marginBottom: "30px",
+          marginLeft: "30%",
           fontSize: "30px",
           letterSpacing: "10px",
         }}
       >
-      <span>C</span>heck
+        <span>C</span>heck
         <span>O</span>ut
         <span>S</span>ome
-        <span>O</span>f
-        <span>M</span>y
-        <span>W</span>orks
+        <span>O</span>f<span>M</span>y<span>W</span>orks
       </h2>
 
       <div className="project-container">
         {product.map((productItem) => {
           return (
             <div className="project-content">
-              <div className="product-item">
-                <div className="project-name">
-                  <p>{productItem.ProjectName}</p>
-                </div>
+              <div className="project-name">
+                <p>{productItem.ProjectName}</p>
+              </div>
 
-                <div className="image-content">
-                  <img src={productItem.image} />
-                </div>
-                <div className="project-desc">
-                  <p> {productItem.Desc} </p>
-                </div>
+              <div className="image-content">
+                <img src={productItem.image} />
+              </div>
+              <div className="project-desc">
+                <p> {productItem.Desc} </p>
               </div>
             </div>
           );
